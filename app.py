f1043104678c5d942e23e86464d26c9f00b211c2
ravitamos <ravitam@post.bgu.ac.il>
2021-12-05 16:20:43 +0200

    ex7

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
-from flask import Flask
+from flask import Flask, redirect,url_for
 
 app = Flask(__name__)
 
 
+@app.route('/home_page')
+@app.route('/home')
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_func():  # put application's code here
+    return 'Hello to home page!'
+
+@app.route('/catalog')
+def catalog_func():
+    return 'Hello to catalog page!'
+
+@app.route('/about')
+def about_func():
+    # todo
+    # do something with db
+    print('i am in about')
+    return redirect('/catalog')
+
+@app.route('/product')
+def product_func():
+    # todo
+    # do something with db
+    print('i am in product page')
+    return redirect(url_for('catalog_func'))
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
